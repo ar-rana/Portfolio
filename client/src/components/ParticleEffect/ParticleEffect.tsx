30531dcbddf2bee80e2d42664e46d697cdb361ce
Aryan Rana <aryan.rana2005@gmail.com>
2025-03-11 20:27:58 +0530

    added logo

--- client/src/components/ParticleEffect/ParticleEffect.tsx
+++ client/src/components/ParticleEffect/ParticleEffect.tsx
@@ -6,29 +6,29 @@ const ParticleEffect: React.FC = () => {
   return (
     <div className={styles.particle_container}>
       <div className={styles.bubble}>
-        <span style={{ "--i": 28 } as React.CSSProperties}></span>
-        <span style={{ "--i": 12 } as React.CSSProperties}></span>
-        <span style={{ "--i": 20 } as React.CSSProperties}></span>
-        <span style={{ "--i": 24 } as React.CSSProperties}></span>
-        <span style={{ "--i": 22 } as React.CSSProperties}></span>
-        <span style={{ "--i": 13 } as React.CSSProperties}></span>
-        <span style={{ "--i": 17 } as React.CSSProperties}></span>
-        <span style={{ "--i": 27 } as React.CSSProperties}></span>
-        <span style={{ "--i": 25 } as React.CSSProperties}></span>
-        <span style={{ "--i": 16 } as React.CSSProperties}></span>
-        <span style={{ "--i": 14 } as React.CSSProperties}></span>
-        <span style={{ "--i": 18 } as React.CSSProperties}></span>
-        <span style={{ "--i": 12 } as React.CSSProperties}></span>
-        <span style={{ "--i": 20 } as React.CSSProperties}></span>
-        <span style={{ "--i": 24 } as React.CSSProperties}></span>
-        <span style={{ "--i": 22 } as React.CSSProperties}></span>
-        <span style={{ "--i": 13 } as React.CSSProperties}></span>
-        <span style={{ "--i": 15 } as React.CSSProperties}></span>
-        <span style={{ "--i": 11 } as React.CSSProperties}></span>
-        <span style={{ "--i": 25 } as React.CSSProperties}></span>
-        <span style={{ "--i": 16 } as React.CSSProperties}></span>
-        <span style={{ "--i": 14 } as React.CSSProperties}></span>
-        <span style={{ "--i": 19 } as React.CSSProperties}></span>
+        <span style={{ "--i": 28 } as React.CSSProperties} className="fa fa-certificate"></span>
+        <span style={{ "--i": 12 } as React.CSSProperties} className="fa fa-asterisk"></span>
+        <span style={{ "--i": 20 } as React.CSSProperties} className="fa fa-asterisk"></span>
+        <span style={{ "--i": 24 } as React.CSSProperties} className="fa fa-certificate"></span>
+        <span style={{ "--i": 22 } as React.CSSProperties} className="fa fa-asterisk"></span>
+        <span style={{ "--i": 13 } as React.CSSProperties} className="fa fa-asterisk"></span>
+        <span style={{ "--i": 17 } as React.CSSProperties} className="fa fa-certificate"></span>
+        <span style={{ "--i": 27 } as React.CSSProperties} className="fa fa-asterisk"></span>
+        <span style={{ "--i": 25 } as React.CSSProperties} className="fa fa-asterisk"></span>
+        <span style={{ "--i": 16 } as React.CSSProperties} className="fa fa-asterisk"></span>
+        <span style={{ "--i": 14 } as React.CSSProperties} className="fa fa-asterisk"></span>
+        <span style={{ "--i": 18 } as React.CSSProperties} className="fa fa-certificate"></span>
+        <span style={{ "--i": 12 } as React.CSSProperties} className="fa fa-certificate"></span>
+        <span style={{ "--i": 20 } as React.CSSProperties} className="fa fa-asterisk"></span>
+        <span style={{ "--i": 24 } as React.CSSProperties} className="fa fa-certificate"></span>
+        <span style={{ "--i": 22 } as React.CSSProperties} className="fa fa-asterisk"></span>
+        <span style={{ "--i": 13 } as React.CSSProperties} className="fa fa-certificate"></span>
+        <span style={{ "--i": 15 } as React.CSSProperties} className="fa fa-certificate"></span>
+        <span style={{ "--i": 11 } as React.CSSProperties} className="fa fa-asterisk"></span>
+        <span style={{ "--i": 25 } as React.CSSProperties} className="fa fa-asterisk"></span>
+        <span style={{ "--i": 16 } as React.CSSProperties} className="fa fa-certificate"></span>
+        <span style={{ "--i": 14 } as React.CSSProperties} className="fa fa-asterisk"></span>
+        <span style={{ "--i": 19 } as React.CSSProperties} className="fa fa-certificate"></span>
       </div>
     </div>
   );
