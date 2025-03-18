810fae6d3439e90937e759c187ed9c20191d7b85
Aryan Rana <aryan.rana2005@gmail.com>
2025-03-18 18:57:44 +0530

    improved BlogCard component

--- client/src/components/BlogCard/BlogCard.tsx
+++ client/src/components/BlogCard/BlogCard.tsx
@@ -1,21 +1,37 @@
 import React from "react";
 import styles from "./blogCard.module.css";
 import { BlogPanel } from "../../../types";
+import PrettyButton from "../PrettyButton/PrettyButton";
 
 interface Blog {
-    blog: BlogPanel;
+  blog: BlogPanel;
 }
-const BlogCard:React.FC<Blog> = ({ blog }) => {
+const BlogCard: React.FC<Blog> = ({ blog }) => {
   return (
-    <div className={styles.blog_card}>
-      <img src={blog.img}/>
-      <div className={styles.blog_info}>
-        <p style={{ fontSize: '18px'}}>{blog.title}</p>
-        <span style={{ fontSize: '14px'}}>{blog.date}</span>
+    // <div className={styles.blog_card}>
+    //   <img src={blog.img}/>
+    //   <div className={styles.blog_info}>
+    //     <p style={{ fontSize: '18px'}}>{blog.title}</p>
+    //     <span style={{ fontSize: '14px'}}>{blog.date}</span>
+    //   </div>
+    //   <span className={styles.blog_details}>
+    //     {blog.desc}
+    //   </span>
+    // </div>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <div className={styles.frontView}>
+          <img src={blog.img} />
+          <div className={styles.text}>
+            <b style={{ fontSize: "20px" }}>{blog.title}</b>
+            <b>{blog.date}</b>
+          </div>
+        </div>
+        <div className={styles.backView}>
+          {blog.desc ? <h4>{blog.desc}</h4> : ""}
+          <PrettyButton text="Visit Article" onClick={() => alert("hello")} />
+        </div>
       </div>
-      <span className={styles.blog_details}>
-        {blog.desc}
-      </span>
     </div>
   );
 };
