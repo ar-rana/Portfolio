e4abccaeaac2b8187e34f0cd026e1b05d7a1d0e7
Aryan Rana <aryan.rana2005@gmail.com>
2025-04-21 19:03:07 +0530

    Updates

--- assets/PrettyButton-CPFcz3h3.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BlwOCxuQ.js";const r="_btn_13w2w_1",e={btn:r},c=({text:n,onClick:s})=>t.jsx(t.Fragment,{children:t.jsx("button",{className:e.btn,onClick:s,children:n})});export{c as P};
