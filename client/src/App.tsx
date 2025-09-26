514647a4300fa01d912a6104270bb9b717371b1a
Aryan Rana <aryan.rana2005@gmail.com>
2025-09-26 10:45:28 +0530

    another attempt for custom domain

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,7 @@ import { ResponseMessage } from "./MessageContext.tsx";
 function App() {
 
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename="/">
     <ResponseMessage>
       <Suspense fallback={<Loading />}>
         <Routes>
