bac5b3419268081d17d93067743b5b16c152e322
Aryan Rana <aryan.rana2005@gmail.com>
2025-09-26 09:53:14 +0530

    changed home page for custom domain

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,7 @@ import { ResponseMessage } from "./MessageContext.tsx";
 function App() {
 
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
     <ResponseMessage>
       <Suspense fallback={<Loading />}>
         <Routes>
